9d5ba1070495b95261634267dc9ced14905e1dbe
Kirttamet <oommylok@gmail.com>
2025-11-23 19:08:58 +0700

    Update Lab 1.1.java

--- Lab 1.1.java
+++ Lab 1.1.java
@@ -3,26 +3,23 @@ package lab1;
 import java.util.Scanner;
 
 public class Main {
-    /**
-     * Lab 1.1: โปรแกรมบวกเลขจำนวนเต็ม 2 จำนวน.
-     */
+
     public static void main(String[] args) {
         
-        // 1. สร้างวัตถุ Scanner เพื่อรับข้อมูลจากคีย์บอร์ด
-        Scanner userInput = new Scanner(System.in); 
 
-        // 2. รับค่าจำนวนเต็มตัวที่หนึ่งและตัวที่สอง
-        // ใช้ชื่อตัวแปรที่ไม่ซ้ำ: valueA, valueB
-        int valueA = userInput.nextInt();
-        int valueB = userInput.nextInt();
+        Scanner ohmuserInput = new Scanner(System.in); 
 
-        // 3. คำนวณผลรวมของทั้งสองจำนวน
-        int resultTotal = valueA + valueB;
 
-        // 4. แสดงผลลัพธ์ออกทางหน้าจอ
+        int ohmvalueA = ohmuserInput.nextInt();
+        int ohmvalueB = ohmuserInput.nextInt();
+
+
+        int resultTotal = ohmvalueA + ohmvalueB;
+
+
         System.out.println(resultTotal);
         
-        // 5. ปิด Scanner
-        userInput.close(); 
+
+        ohmuserInput.close(); 
     }
 }
